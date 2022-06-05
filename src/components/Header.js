9b301d674406e52ef3641133a972be7f4f0d9a87
hoangnym <hoangnym94@gmail.com>
2022-06-05 16:26:56 +0200

    start header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
-  return <div>header</div>;
+  return <Container>header</Container>;
 }
 
 export default Header;
+
+const Container = styled.div``;
